3332a1a8aa608d9cd21ea29c46d22e9dd0cee14f
gbmello <gabriel_meello@hotmail.com>
2024-09-12 10:10:20 -0300

    feat - Second commit - add RabbitmQ config

---
+++ src/main/java/com/gabrielmello/apibtgpactual/config/RabbitMqConfig.java
@@ -0,0 +1,11 @@
+package com.gabrielmello.apibtgpactual.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+}
