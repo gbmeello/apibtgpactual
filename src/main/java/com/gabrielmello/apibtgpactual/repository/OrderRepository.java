cd8c6b3513e920d324b98184bbeca345b3747257
gbmello <gabriel_meello@hotmail.com>
2024-09-12 11:26:39 -0300

    feat - fifth commit - created repository and service

---
+++ src/main/java/com/gabrielmello/apibtgpactual/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.gabrielmello.apibtgpactual.repository;
+
+import com.gabrielmello.apibtgpactual.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<OrderEntity, Long> {
+    Page<OrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
+
+}
