3251306c98972f050e4dde43de0725683112ad81
gbmello <gabriel_meello@hotmail.com>
2024-09-12 09:39:10 -0300

    feat - initial commit add docker-compose

---
+++ src/main/java/com/gabrielmello/apibtgpactual/ApibtgpactualApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielmello.apibtgpactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibtgpactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibtgpactualApplication.class, args);
+	}
+
+}
